34e5c2542cf0f301fb3528f01422ee52d284a853
Shawn Rivers <kaushou@gmail.com>
2019-02-02 11:17:44 +0900

    Add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,32 @@
+export enum CdsCurrentPage {
+  Single = "singles",
+  Album = "albums",
+}
+
+export enum CdType {
+  A = "A",
+  B = "B",
+  C = "C",
+  D = "D",
+  T = "T",
+  L = "Limited"
+}
+
+export enum SongType {
+  None = "none",
+  Title = "title",
+  Coupling = "coupling",
+  Under = "under",
+  Unit = "unit",
+  Solo = "solo",
+  FirstGeneration = "first generation",
+  SecondGeneration = "second generation",
+  ThirdGeneration = "third generation"
+}
+
+export enum FocusPerformersType {
+  Center = "center",
+  Solo = "solo",
+  Unit = "unit",
+  None = ""
+}
