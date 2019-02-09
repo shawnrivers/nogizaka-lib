8da21ebe23982230bf30d5ee82801ba51e99cfd5
Shawn Rivers <kaushou@gmail.com>
2019-02-09 17:14:28 +0900

    Updates

---
+++ precache-manifest.d9a91a5d91c5c3d762df2a0186983bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cfb2b99b7e48a97feb9",
+    "url": "/nogizaka-lib/static/js/runtime~main.0cfb2b99.js"
+  },
+  {
+    "revision": "bd47bc18e5717ba0eac8",
+    "url": "/nogizaka-lib/static/js/main.bd47bc18.chunk.js"
+  },
+  {
+    "revision": "92fd9c9db60c09ac0e9f",
+    "url": "/nogizaka-lib/static/js/1.92fd9c9d.chunk.js"
+  },
+  {
+    "revision": "bd47bc18e5717ba0eac8",
+    "url": "/nogizaka-lib/static/css/main.6f20eee8.chunk.css"
+  },
+  {
+    "revision": "efe019a93401183f575afa7211d000e3",
+    "url": "/nogizaka-lib/index.html"
+  }
+];
