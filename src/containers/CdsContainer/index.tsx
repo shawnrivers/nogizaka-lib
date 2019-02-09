ffd44d0c97bfa45edc1f4aad4fe544e7d1c6ca7d
Shawn Rivers <kaushou@gmail.com>
2019-02-09 16:53:07 +0900

    Remove unused useState

--- src/containers/CdsContainer/index.tsx
+++ src/containers/CdsContainer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Cds } from "../../components/templates/Cds";
 import { switchCdsPage, fetchSingles, fetchAlbums } from "./store/actions";
