1d70ed7cac3b787a48573cfa2c8dc419e36aa0c5
Shawn Rivers <kaushou@gmail.com>
2019-02-09 18:34:04 +0900

    Fix router basename issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import * as React from "react";
+import React from "react";
 import { Route, Redirect, Switch } from "react-router";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-import { CdsContianer, ICdsContainerProps } from "./containers/CdsContainer";
+import { CdsContianer } from "./containers/CdsContainer";
 import "./App.scss";
 import { store } from "./stores/appStore";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter basename="nogizaka-lib" >
         <div className="app">
-          <CdsContianer {...{} as ICdsContainerProps} />
+          <Switch>
+            <Route exact path="/cds" component={CdsContianer} />
+            <Redirect exact from="/" to="/cds" />
+          </Switch>
         </div>
-      </Provider>
-    );
-  }
-}
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
