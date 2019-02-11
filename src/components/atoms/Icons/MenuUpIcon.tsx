60e2b8cf2eab3b6736f3658b6086b45dc26a8a3e
Shawn Rivers <kaushou@gmail.com>
2019-02-11 12:59:09 +0900

    Add dropdown menu icons

---
+++ src/components/atoms/Icons/MenuUpIcon.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const MenuUpIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+);
