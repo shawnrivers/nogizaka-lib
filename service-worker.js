1bc2fd5941c45b10480d9d35d6252c9e02fbebfc
Shawn Rivers <kaushou@gmail.com>
2019-03-16 00:17:39 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nogizaka-lib/precache-manifest.e8d635fe365a2a386936f09725185315.js"
+  "/nogizaka-lib/precache-manifest.862c83331b6c643cfe4e5e70cfe49396.js"
 );
 
 workbox.clientsClaim();
