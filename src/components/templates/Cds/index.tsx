5f6d32b01102a73834bf2397dd396f7cc1740179
Shawn Rivers <kaushou@gmail.com>
2019-03-25 02:03:47 +0900

    Replace pose with react-transition-group in Cds

--- src/components/templates/Cds/index.tsx
+++ src/components/templates/Cds/index.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import styles from './Cds.module.scss';
-import { ISingle } from '../../../models/ISingle';
-import { IAlbum } from '../../../models/IAlbum';
 import { ICdsState } from '../../../containers/CdsContainer/store/reducers';
 import { CdCardList } from '../../organisms/CdCardList';
 import { TopLeftBackground } from '../../atoms/Background/TopLeftBackground';
@@ -18,17 +15,6 @@ interface ICdsProps extends RouteComponentProps<IMatchParams> {
   fetchAlbums(): void;
 }
 
-const getCurrentPageCds = (cdsCurrentPage: CdsCurrentPage, cds: ICdsState): (ISingle | IAlbum)[] => {
-  switch (cdsCurrentPage) {
-    case CdsCurrentPage.Single:
-      return cds.singles.data;
-    case CdsCurrentPage.Album:
-      return cds.albums.data;
-    default:
-      return [];
-  }
-};
-
 export const Cds = (props: ICdsProps) => {
   React.useEffect(() => {
     if (props.cds.singles.fetchStatus === FetchStatus.None) {
@@ -41,13 +27,12 @@ export const Cds = (props: ICdsProps) => {
 
   const pages = [CdsCurrentPage.Single, CdsCurrentPage.Album];
   const currentPage = props.match.params.type;
-  const cdsContents = React.useMemo(() => getCurrentPageCds(currentPage, props.cds), [currentPage, props.cds]);
 
   return (
-    <div className={styles.container}>
+    <div>
       <TopLeftBackground />
       <TabMenu items={pages} currentPage={currentPage} />
-      <CdCardList cds={cdsContents} currentPage={currentPage} />
+      <CdCardList singles={props.cds.singles.data} albums={props.cds.albums.data} currentPage={currentPage} />
     </div>
   );
 };
