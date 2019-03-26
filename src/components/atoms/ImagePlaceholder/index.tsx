3ca1e3e856c16953b743d44c2a84ba63156a59ea
Shawn Rivers <kaushou@gmail.com>
2019-03-26 09:51:01 +0900

    Lazy-load images

---
+++ src/components/atoms/ImagePlaceholder/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import styles from './ImagePlaceholder.module.scss';
+
+export const ImagePlaceholder = () => <div className={styles.container} />;
--- src/components/molecules/CdCard/index.tsx
+++ src/components/molecules/CdCard/index.tsx
@@ -2,19 +2,29 @@ import * as React from 'react';
 import styles from './CdCard.module.scss';
 import { ISingle } from '../../../models/ISingle';
 import { IAlbum } from '../../../models/IAlbum';
+import LazyLoad, { forceCheck } from 'react-lazyload';
+import { ImagePlaceholder } from '../../atoms/ImagePlaceholder';
 
 interface ICdProps {
   cd: ISingle | IAlbum;
 }
 
-export const CdCard = (props: ICdProps) => (
-  <div className={styles.container}>
-    <img className={styles.artwork} src={props.cd.artworks[0].urls.medium} alt={props.cd.title} />
-    <div className={styles.text}>
-      <span>
-        <span className={styles.number}>{props.cd.number}.</span>
-        <span className={styles.title}>{props.cd.title}</span>
-      </span>
+export const CdCard = (props: ICdProps) => {
+  React.useEffect(() => {
+    forceCheck();
+  });
+
+  return (
+    <div className={styles.container}>
+      <LazyLoad placeholder={<ImagePlaceholder />} offset={100}>
+          <img className={styles.artwork} src={props.cd.artworks[0].urls.medium} alt={props.cd.title} />
+      </LazyLoad>
+      <div className={styles.text}>
+        <span>
+          <span className={styles.number}>{props.cd.number}.</span>
+          <span className={styles.title}>{props.cd.title}</span>
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
