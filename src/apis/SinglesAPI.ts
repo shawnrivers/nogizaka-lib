04c04f5d6699f877c8db01dbf6f2d76e573871e0
Shawn Rivers <kaushou@gmail.com>
2019-04-02 23:10:22 +0900

    Add an API client for fetchSingles

---
+++ src/apis/SinglesAPI.ts
@@ -0,0 +1,27 @@
+import { fetchGet } from '../utils/fetch';
+import { SingleResponse } from './responseTypes';
+import { ISingle } from '../models/ISingle';
+
+const convertSingleResponse = (singleResponse: SingleResponse): ISingle => {
+  const { number, title, release, artworks, shopping, songs, behindPerformers } = singleResponse;
+
+  return {
+    number,
+    title,
+    release,
+    artworks,
+    shopping,
+    songs,
+    behindPerformers,
+  };
+};
+
+export const fetchSingles = async (): Promise<ISingle[]> => {
+  const singlesResponse: SingleResponse[] = await fetchGet(
+    'https://raw.githubusercontent.com/shawnrivers/nogizaka-data/master/src/json/singles.json',
+  );
+
+  const convertedSingles = singlesResponse.map(singleResponse => convertSingleResponse(singleResponse));
+
+  return convertedSingles;
+};
