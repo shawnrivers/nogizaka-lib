231fbb8d05535aae11b05e6cbaadba01106ea925
Shawn Rivers <kaushou@gmail.com>
2019-04-19 17:57:25 +0900

    Add links to navigation bar

--- src/components/molecules/NavigationBar/index.tsx
+++ src/components/molecules/NavigationBar/index.tsx
@@ -3,6 +3,7 @@ import styles from './NavigationBar.module.scss';
 import { CdIcon } from '../../atoms/Icons/CdIcon';
 import { MembersIcon } from '../../atoms/Icons/MembersIcon';
 import { SearchIcon } from '../../atoms/Icons/SearchIcon';
+import { Link } from 'react-router-dom';
 
 interface INavigationBarProps {
   currentTab: 'cds' | 'members' | 'search';
@@ -11,20 +12,24 @@ interface INavigationBarProps {
 export const NavigationBar = (props: INavigationBarProps) => {
   return (
     <div className={styles.navigation}>
-      <button className={styles['button']}>
-        {props.currentTab === 'cds' ? (
-          <CdIcon className={styles['cd-icon-active']} isActive={true} />
-        ) : (
-          <CdIcon className={styles['cd-icon-inactive']} isActive={false} />
-        )}
-      </button>
-      <button className={styles['button']}>
-        {props.currentTab === 'members' ? (
-          <MembersIcon className={styles['cd-icon-active']} isActive={true} />
-        ) : (
-          <MembersIcon className={styles['cd-icon-inactive']} isActive={false} />
-        )}
-      </button>
+      <Link to="/cds/singles">
+        <button className={styles['button']}>
+          {props.currentTab === 'cds' ? (
+            <CdIcon className={styles['cd-icon-active']} isActive={true} />
+          ) : (
+            <CdIcon className={styles['cd-icon-inactive']} isActive={false} />
+          )}
+        </button>
+      </Link>
+      <Link to="/members/first">
+        <button className={styles['button']}>
+          {props.currentTab === 'members' ? (
+            <MembersIcon className={styles['cd-icon-active']} isActive={true} />
+          ) : (
+            <MembersIcon className={styles['cd-icon-inactive']} isActive={false} />
+          )}
+        </button>
+      </Link>
       <button className={styles['button']}>
         {props.currentTab === 'search' ? (
           <SearchIcon className={styles['cd-icon-active']} />
