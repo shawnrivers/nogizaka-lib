cfb3e3a348bd8ff63d93a9386c7b326cd245fe4a
Shawn Rivers <kaushou@gmail.com>
2019-04-27 03:06:17 +0900

    Add selectors for selecting CD by its title

--- src/containers/Cds/selectors.ts
+++ src/containers/Cds/selectors.ts
@@ -1,7 +1,7 @@
-import { IRootState } from "../../stores/state";
-import { ISingles, ISingle } from "../../models/ISingle";
-import { IAlbums, IAlbum } from "../../models/IAlbum";
-import { FetchStatus } from "../../utils/constants";
+import { IRootState } from '../../stores/state';
+import { ISingles, ISingle } from '../../models/ISingle';
+import { IAlbums, IAlbum } from '../../models/IAlbum';
+import { FetchStatus } from '../../utils/constants';
 
 export const selectSingles = (state: IRootState): ISingles => state.cds.singles.data;
 export const selectAlbums = (state: IRootState): IAlbums => state.cds.albums.data;
@@ -11,3 +11,6 @@ export const selectAlbumArray = (state: IRootState): IAlbum[] => Object.values(s
 
 export const selectSinglesFetchStatus = (state: IRootState): FetchStatus => state.cds.singles.fetchStatus;
 export const selectAlbumsFetchStatus = (state: IRootState): FetchStatus => state.cds.albums.fetchStatus;
+
+export const selectSingleByTitle = (state: IRootState, title: string): ISingle => selectSingles(state)[title];
+export const selectAlbumByTitle = (state: IRootState, title: string): IAlbum => selectAlbums(state)[title];
