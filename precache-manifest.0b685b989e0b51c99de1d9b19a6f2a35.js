7b4f8901406b55bee715788dc82c9bbef27aa354
Shawn Rivers <kaushou@gmail.com>
2019-04-29 17:12:06 +0900

    Updates

--- precache-manifest.0b685b989e0b51c99de1d9b19a6f2a35.js
+++ precache-manifest.0b685b989e0b51c99de1d9b19a6f2a35.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/nogizaka-lib/static/js/runtime~main.0cfb2b99.js"
   },
   {
-    "revision": "829137136539633bc9a3",
-    "url": "/nogizaka-lib/static/js/main.82913713.chunk.js"
+    "revision": "d8c375b03cffd4554dbd",
+    "url": "/nogizaka-lib/static/js/main.d8c375b0.chunk.js"
   },
   {
     "revision": "c20719d38db201cb6d21",
     "url": "/nogizaka-lib/static/js/1.c20719d3.chunk.js"
   },
   {
-    "revision": "829137136539633bc9a3",
+    "revision": "d8c375b03cffd4554dbd",
     "url": "/nogizaka-lib/static/css/main.6b535451.chunk.css"
   },
   {
-    "revision": "77bc3063f8ce340ca46daff99af05e41",
+    "revision": "7ea88ed32b90e70c28c2ded28311778a",
     "url": "/nogizaka-lib/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nogizaka-lib/precache-manifest.80420579d32e5e243fb375bb82f00e24.js"
+  "/nogizaka-lib/precache-manifest.0b685b989e0b51c99de1d9b19a6f2a35.js"
 );
 
 workbox.clientsClaim();
