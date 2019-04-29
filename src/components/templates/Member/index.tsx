5f35c30dbe773cdf08a02279549027948227b1de
Shawn Rivers <kaushou@gmail.com>
2019-04-29 12:18:09 +0900

    Add basic info and photo albums info

--- src/components/templates/Member/index.tsx
+++ src/components/templates/Member/index.tsx
@@ -32,6 +32,38 @@ export const Member = (props: IMemberProps) => {
             srcSet={`${props.member.profileImage.small}, ${props.member.profileImage.large} 2x`}
           />
         </LazyLoad>
+        <DetailsCard className={styles.card}>
+          <div className={styles['first-container']}>
+            <span className={styles.name}>
+              {props.member.nameNotations.lastName + ' ' + props.member.nameNotations.firstName}
+            </span>
+            <span className={styles['name-caption']}>
+              {props.member.nameNotations.lastNameFurigana + ' ' + props.member.nameNotations.firstNameFurigana} |{' '}
+              {props.member.nameNotations.lastNameEn + ' ' + props.member.nameNotations.firstNameEn}
+            </span>
+            <span className={styles.sites}>
+              {props.member.sites.map(site => (
+                <a href={site.url} key={site.title} target="_blank">
+                  {site.title}
+                </a>
+              ))}
+            </span>
+          </div>
+          <Divider />
+          {props.member.photoAlbums.length > 0 ? (
+            <div className={styles['photo-albums-container']}>
+              <span className={styles['sub-heading']}>写真集 / Photo Books</span>
+              <ol className={styles['photo-album-list']}>
+                {props.member.photoAlbums.map(photoAlbum => (
+                  <li key={photoAlbum.title} className={styles['photo-album']}>
+                    <span>『{photoAlbum.title}』</span>{' '}
+                    <span className={styles['photo-album-release']}>({photoAlbum.release})</span>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : null}
+        </DetailsCard>
       </main>
     </>
   ) : null;
