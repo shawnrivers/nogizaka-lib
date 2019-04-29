094dff162a1a8ebb8459528e0712599906b6dd12
Shawn Rivers <kaushou@gmail.com>
2019-04-29 12:13:48 +0900

    Make DetailsCard capable of taking className prop

--- src/components/molecules/DetailsCard/index.tsx
+++ src/components/molecules/DetailsCard/index.tsx
@@ -3,6 +3,9 @@ import styles from './DetailsCard.module.scss';
 
 interface IDetailsCardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const DetailsCard = (props: IDetailsCardProps) => <div className={styles.container}>{props.children}</div>;
+export const DetailsCard = (props: IDetailsCardProps) => (
+  <div className={`${styles.container} ${props.className}`}>{props.children}</div>
+);
