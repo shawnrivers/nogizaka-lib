ed4a1acf68a5d95598ceb8bf9d38675177663ff0
Shawn Rivers <kaushou@gmail.com>
2019-04-30 04:09:24 +0900

    Add song page artwork and basic info

--- src/components/templates/Song/index.tsx
+++ src/components/templates/Song/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
-import { ISong } from '../../../models/ISong';
+import { ISongDisplay } from '../../../models/ISong';
 import { FetchStatus } from '../../../utils/constants';
+import { useScrollRestoration } from '../../../utils/hooks';
+import { TitleBar } from '../../molecules/TitleBar';
+import { ArtworkCarousel } from '../../molecules/ArtworkCarousel';
+import { DetailsCard } from '../../molecules/DetailsCard';
+import { Divider } from '../../atoms/Divider';
 import styles from './Song.module.scss';
 
 export type ISongVariableProps = {
@@ -26,5 +31,60 @@ export const Song = (props: ISongProps) => {
     }
   }, []);
 
-  return <div />;
+  useScrollRestoration();
+
+  return props.song !== undefined ? (
+    <>
+      <TitleBar title={props.song.title} />
+      <main>
+        <ArtworkCarousel artwork={props.song.artwork.large} />
+        <DetailsCard className={styles.card}>
+          <div className={styles.heading}>
+            <span className={styles.title}>{props.song.title}</span>
+            <div className={styles.hashtags}>
+              <span className={styles.hashtag}>#{props.song.type}</span>
+              {props.song.single.title !== '' ? (
+                <span className={styles.hashtag}>#{props.song.single.number} Single</span>
+              ) : null}
+              {props.song.albums.length > 0
+                ? props.song.albums.map(album => (
+                    <span className={styles.hashtag} key={album.number}>
+                      #{album.number} Album
+                    </span>
+                  ))
+                : null}
+            </div>
+          </div>
+          <div className={styles['creators-container']}>
+            {props.song.creators.lyrics.length > 0 ? (
+              <div className={styles['creator-info']}>
+                <span className={styles['creator-title']}>作詞</span>
+                <span className={styles['creator-content']}>{props.song.creators.lyrics.join(', ')}</span>
+              </div>
+            ) : null}
+
+            {props.song.creators.compose.length > 0 ? (
+              <div className={styles['creator-info']}>
+                <span className={styles['creator-title']}>作曲</span>
+                <span className={styles['creator-content']}>{props.song.creators.compose.join(', ')}</span>
+              </div>
+            ) : null}
+            {props.song.creators.arrange.length > 0 ? (
+              <div className={styles['creator-info']}>
+                <span className={styles['creator-title']}>編曲</span>
+                <span className={styles['creator-content']}>{props.song.creators.arrange.join(', ')}</span>
+              </div>
+            ) : null}
+            {props.song.creators.direct.length > 0 ? (
+              <div className={styles['creator-info']}>
+                <span className={styles['creator-title']}>MV監督</span>
+                <span className={styles['creator-content']}>{props.song.creators.direct.join(', ')}</span>
+              </div>
+            ) : null}
+          </div>
+          <Divider />
+        </DetailsCard>
+      </main>
+    </>
+  ) : null;
 };
