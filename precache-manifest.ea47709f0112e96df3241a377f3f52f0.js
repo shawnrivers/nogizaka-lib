918de2efb7a44610a9d3c978796785599128e367
Shawn Rivers <kaushou@gmail.com>
2019-04-30 17:18:10 +0900

    Updates

---
+++ precache-manifest.ea47709f0112e96df3241a377f3f52f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cfb2b99b7e48a97feb9",
+    "url": "/nogizaka-lib/static/js/runtime~main.0cfb2b99.js"
+  },
+  {
+    "revision": "1f60501d9eefe157ea93",
+    "url": "/nogizaka-lib/static/js/main.1f60501d.chunk.js"
+  },
+  {
+    "revision": "6b7f084dcaae29e5c415",
+    "url": "/nogizaka-lib/static/js/1.6b7f084d.chunk.js"
+  },
+  {
+    "revision": "1f60501d9eefe157ea93",
+    "url": "/nogizaka-lib/static/css/main.8eb0a8ac.chunk.css"
+  },
+  {
+    "revision": "1cbf584b00160868969a2a23d2ecdc6b",
+    "url": "/nogizaka-lib/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nogizaka-lib/precache-manifest.f1c190c7615b0943729c6f828bdc11c3.js"
+  "/nogizaka-lib/precache-manifest.ea47709f0112e96df3241a377f3f52f0.js"
 );
 
 workbox.clientsClaim();
