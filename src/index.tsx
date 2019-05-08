aacf8b066c553284209423eb086b3433521fb471
Shawn Rivers <kaushou@gmail.com>
2019-05-09 00:48:56 +0900

    Add pre-rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-
+import { hydrate, render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (rootElement !== null && rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, document.getElementById('root'));
+}
 
 serviceWorker.register();
