5e99f5af14f9bb1e4b158127c83e594e009cbce9
Shawn Rivers <kaushou@gmail.com>
2019-05-10 23:02:38 +0900

    Remove pre-rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement !== null && rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, document.getElementById('root'));
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
