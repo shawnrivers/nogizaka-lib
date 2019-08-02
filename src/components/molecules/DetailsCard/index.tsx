133061f94acc6c0404729ddf57735e166dab3bcf
Shawn Rivers <kaushou@gmail.com>
2019-08-02 23:49:09 +0900

    Add gradient background to details card

--- src/components/molecules/DetailsCard/index.tsx
+++ src/components/molecules/DetailsCard/index.tsx
@@ -4,8 +4,15 @@ import styles from './DetailsCard.module.scss';
 interface IDetailsCardProps {
   children: React.ReactNode;
   className?: string;
+  hasBackground?: boolean;
 }
 
 export const DetailsCard = (props: IDetailsCardProps) => (
-  <div className={`${styles.container} ${props.className}`}>{props.children}</div>
+  <div
+    className={`${props.hasBackground ? styles['container-with-background'] : styles['container-without-background']} ${
+      props.className
+    }`}
+  >
+    {props.children}
+  </div>
 );
